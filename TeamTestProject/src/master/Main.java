274d0a2bfd294f2005748e7171cad26a80e25a40
User <User@DESKTOP-CCQJ08D>
2018-12-20 20:13:11 +0100

    Zweiter Commit and Push

---
+++ TeamTestProject/src/master/Main.java
@@ -0,0 +1,14 @@
+package master;
+
+//Ein erster GitHub Team Projekt Test.
+//Die Premium-Version kostet 7.00€ pro Monat.
+
+public class Main {
+	
+	private static final int iq = 0;
+	
+	public static void main(String[] args) {
+		System.out.println(iq);
+	}
+	
+}
